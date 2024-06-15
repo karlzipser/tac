106ccde7ab4cd9bf44a56060eeac58de735a60ce
Karl Zipser <karlzipser@berkeley.edu>
2024-06-14 23:36:52 -0700

    gacp

--- code/main.py
+++ code/main.py
@@ -1,4 +1,4 @@
-
+# python3 utilz2/dev/project.py --src tac --tag with_net
 if __name__ == '__main__':
     import sys
     print("Argument List:", str(sys.argv))
--- params/a.py
+++ params/a.py
@@ -1,4 +1,4 @@
-batch_size=16
+batch_size=32
 num_workers=4
-num_epochs=6
+num_epochs=10
 device='cpu'
