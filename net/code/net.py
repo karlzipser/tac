c055d54f50a19258e11bb416411492cf3bc3f2a7
Karl Zipser <karlzipser@berkeley.edu>
2024-06-15 01:13:56 -0700

    gacp

--- net/code/net.py
+++ net/code/net.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+net_path=__file__
 
 class Net(nn.Module):
     def __init__(self):
@@ -21,5 +22,26 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+if False:
+    from project_tac.a14Jun24_23h26m41s_with_net.tac.net.code.net import *
+    w=most_recent_file_in_folder(opj(pname(pname(net_path)),'weights'))
+    from UTILS_.vis import *
+    #w='/Users/karlzipser/project_tac/14Jun24_23h35m34s_with_net/tac/net/weights/14Jun24_23h35m37s.pth'
+    net=get_net(Net,weights_file=w)
+
+if False:
+    # load net given only the project path minimal information
+    import importlib.util
+    from UTILS_.vis import * # import this from local env
+    file_path = '/Users/karlzipser/project_tac/a14Jun24_23h26m41s_with_net'
+    # add the rest of the paths for net.py and weights '/net/code/net.py'
+    module_name = 'module'
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    w=most_recent_file_in_folder(opj(pname(pname(module.net_path)),'weights'))
+    net=get_net(module.Net,weights_file=w)
 
 #EOF
