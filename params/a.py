e0724d19f3f65ce1bfa098dd791db222a27f27e8
Karl Zipser <karl.zipser.k@gmail.com>
2024-06-18 21:11:20 -0700

    gacp

--- params/a.py
+++ params/a.py
@@ -9,9 +9,9 @@ p=k2c(
 	loss_time=10,
 	test_time=60,
 	lr=0.0001,
-	percent_loss_to_show=25,
+	percent_loss_to_show=100,
 	loss_s=.01,
-	run_path='project_tac/18Jun24_13h59m32s',
+	run_path='project_tac/18Jun24_14h09m36s_long_train' #'project_tac/18Jun24_13h59m32s',
 )
 
 #EOF
