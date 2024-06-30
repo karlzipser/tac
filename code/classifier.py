81a4750c728f89ce4e66e9ecb8f59f6b2d39154e
Karl Zipser <karlzipser@berkeley.edu>
2024-06-30 01:11:10 -0700

    gacp

--- code/classifier.py
+++ code/classifier.py
@@ -98,209 +98,209 @@ print('*** Start Training . . .')
 
 
 for ig in range(10**20):
-    
-    if p.timer.max.check():
-        break
-    if p.timer.epoch.rcheck():
-        cE('epoch')
-        for k in p.data_recorders:
-            p.data_recorders[k].dataloader=loader_dic[p.data_recorders[k].dataloader]
-
-
-    if printr_timer.rcheck():
-        l=[]
-        for task in p.data_recorders:
-            l.append(len(p.data_recorders[task].processed))
-        l=tuple(l)
-        printr(d2n(thispath.replace(opjh(),''),': ig=',ig,', t=',int(p.timer.max.time()),'s processed=',l))
-
-    task=np.random.choice(p.task_list)
-
     try:
-        inputs,labels=next(p.data_recorders[task].dataiter)
-    except:
-        p.data_recorders[task].dataiter=iter(p.data_recorders[task].dataloader)
-        inputs,labels=next(p.data_recorders[task].dataiter)
-    inputs=inputs.to(device)
+        if p.timer.max.check():
+            break
+        if p.timer.epoch.rcheck():
+            cE('epoch')
+            for k in p.data_recorders:
+                p.data_recorders[k].dataloader=loader_dic[p.data_recorders[k].dataloader]
 
 
-    #
-    ##########################################################################
-    #
-    if p.data_recorders[task].noise_level and rnd()<p.data_recorders[task].noise_p:
-        inputs+=rnd()*p.data_recorders[task].noise_level*torch.randn(
-            inputs.size()).to(device)
+        if printr_timer.rcheck():
+            l=[]
+            for task in p.data_recorders:
+                l.append(len(p.data_recorders[task].processed))
+            l=tuple(l)
+            printr(d2n(thispath.replace(opjh(),''),': ig=',ig,', t=',int(p.timer.max.time()),'s processed=',l))
 
-    if 'test' not in p.data_recorders[task].name:
-        assert 'train' in p.data_recorders[task].name
-        net.train()
-        optimizer.zero_grad()
-    else:
-        net.eval()
-    
-    outputs = net(inputs)
-    targets=0*outputs.detach()
-    if not p.data_recorders[task].targets_to_zero:
-        for i in range(targets.size()[0]):
-            targets[i,labels[i],0,0]=1
-    
-    loss = p.criterion(torch.flatten(outputs,1),torch.flatten(targets,1))
-    if 'test' not in p.data_recorders[task].name:
-        loss.backward()
-        optimizer.step()
+        task=np.random.choice(p.task_list)
 
-    #cE(inputs.size(),outputs.size(),targets.size())
-    #
-    ##########################################################################
-    # 
-    _d=dict(
-            ig=ig,
-            inputs=inputs.detach().cpu(),
-            labels=labels.detach().cpu(),
-            outputs=outputs.detach().cpu(),
-            targets=targets.detach().cpu(),
-            loss=loss.detach().cpu()
-        )
+        try:
+            inputs,labels=next(p.data_recorders[task].dataiter)
+        except:
+            p.data_recorders[task].dataiter=iter(p.data_recorders[task].dataloader)
+            inputs,labels=next(p.data_recorders[task].dataiter)
+        inputs=inputs.to(device)
 
-    p.data_recorders[task].add(_d)
 
-    #
-    ##########################################################################
-    #    
-
-    if p.timer.show.rcheck():
-        for task in p.data_recorders:
-            #cb(task)
-            latest=p.data_recorders[task].latest()
-            if not latest:
-                continue
-            show_sample_outputs(
-                latest['inputs'],
-                latest['outputs'],
-                latest['labels'],
-                ig,
-                p.data_recorders[task].name+'_outputs',
-                save_path=opj(paths.figures),
-                )
-            sh(torchvision.utils.make_grid(latest['inputs']),
-                title=p.data_recorders[task].name+'_examples',use_spause=False,
-                    save_path=opj(paths.figures))
         #
-        ######################################################################
+        ##########################################################################
+        #
+        if p.data_recorders[task].noise_level and rnd()<p.data_recorders[task].noise_p:
+            inputs+=rnd()*p.data_recorders[task].noise_level*torch.randn(
+                inputs.size()).to(device)
+
+        if 'test' not in p.data_recorders[task].name:
+            assert 'train' in p.data_recorders[task].name
+            net.train()
+            optimizer.zero_grad()
+        else:
+            net.eval()
+        
+        outputs = net(inputs)
+        targets=0*outputs.detach()
+        if not p.data_recorders[task].targets_to_zero:
+            for i in range(targets.size()[0]):
+                targets[i,labels[i],0,0]=1
+        
+        loss = p.criterion(torch.flatten(outputs,1),torch.flatten(targets,1))
+        if 'test' not in p.data_recorders[task].name:
+            loss.backward()
+            optimizer.step()
+
+        #cE(inputs.size(),outputs.size(),targets.size())
+        #
+        ##########################################################################
+        # 
+        _d=dict(
+                ig=ig,
+                inputs=inputs.detach().cpu(),
+                labels=labels.detach().cpu(),
+                outputs=outputs.detach().cpu(),
+                targets=targets.detach().cpu(),
+                loss=loss.detach().cpu()
+            )
+
+        p.data_recorders[task].add(_d)
+
+        #
+        ##########################################################################
         #    
-        from sklearn.metrics import f1_score
-        from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-        save_path=opj(paths.figures)
 
-        if True:#try:
-
+        if p.timer.show.rcheck():
             for task in p.data_recorders:
-
-                processed=p.data_recorders[task].processed
-                n=max(1,len(processed)//100)
-                if not len(processed):
+                #cb(task)
+                latest=p.data_recorders[task].latest()
+                if not latest:
                     continue
-                figure(1);clf()
-                for c in classes:
+                show_sample_outputs(
+                    latest['inputs'],
+                    latest['outputs'],
+                    latest['labels'],
+                    ig,
+                    p.data_recorders[task].name+'_outputs',
+                    save_path=opj(paths.figures),
+                    )
+                sh(torchvision.utils.make_grid(latest['inputs']),
+                    title=p.data_recorders[task].name+'_examples',use_spause=False,
+                        save_path=opj(paths.figures))
+            #
+            ######################################################################
+            #    
+            from sklearn.metrics import f1_score
+            from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+            save_path=opj(paths.figures)
+
+            if True:#try:
+
+                for task in p.data_recorders:
+
+                    processed=p.data_recorders[task].processed
+                    n=max(1,len(processed)//100)
+                    if not len(processed):
+                        continue
+                    figure(1);clf()
+                    for c in classes:
+                        f=[]
+                        igs=[]
+                        for pr in processed:
+                            f.append(pr['accuracy'][classes[c]])
+                            igs.append(pr['ig'])
+                        x=moving_average(igs,n)
+                        y=moving_average(f,n)
+                        plot(x,y,label=classes[c])
+                    plt.title(p.data_recorders[task].name+' accuracy')
+                    plt.legend(kys(classes),loc='upper left')
+                    plt.savefig(
+                        opj(save_path,
+                            p.data_recorders[task].name+'-'+get_safe_name(
+                                'accuracy')+'.pdf'),
+                        bbox_inches='tight')
+
+
+                    figure(1);clf()
+                    for c in classes:
+                        f=[]
+                        igs=[]
+                        for pr in processed:
+                            f.append(pr['f1_scores'][classes[c]])
+                            igs.append(pr['ig'])
+                        x=moving_average(igs,n)
+                        y=moving_average(f,n)
+                        plot(x,y,label=classes[c])
+                    plt.title(p.data_recorders[task].name+' f1-scores')
+                    plt.legend(kys(classes),loc='upper left')
+                    plt.savefig(
+                        opj(save_path,p.data_recorders[
+                            task].name+'-'+get_safe_name(
+                                'f1-scores')+'.pdf'),
+                        bbox_inches='tight')
+
+
+                    try:
+                        if len(processed)>1:
+                            fig=figure(1);clf()
+                            ax=fig.add_subplot(111)
+                            a=max(1,int(len(processed)*0.9))
+                            b=len(processed)
+                            c=[]
+                            for i in range(a,b):
+                                c.append(processed[i]['confusion_matrix'])
+                            c=na(c)
+                            c=c.sum(axis=0)
+                            c=np.round((100*c.astype(
+                                'float')/c.sum(axis=1)[:,np.newaxis])).astype(int)
+
+                            disp=ConfusionMatrixDisplay(
+                                confusion_matrix=c,
+                                display_labels=kys(classes))
+                            disp.plot(ax=ax,cmap=plt.cm.Blues)
+                            plt.title(
+                                d2s(p.data_recorders[task].name,'confusion_matrix',ig))
+                            plt.savefig(
+                                opj(save_path,
+                                    p.data_recorders[task].name+'-'+get_safe_name(
+                                        'confusion_matrix.pdf')),
+                                bbox_inches='tight')
+                    except KeyboardInterrupt:
+                        cr('*** KeyboardInterrupt ***')
+                        sys.exit()
+                    except Exception as e:
+                        exc_type, exc_obj, exc_tb = sys.exc_info()
+                        file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                        print('Exception!')
+                        print(d2s(exc_type,file_name,exc_tb.tb_lineno)) 
+
+
+
+                figure(1)
+                clf()
+                for task in p.data_recorders:
+                    processed=p.data_recorders[task].processed
                     f=[]
                     igs=[]
                     for pr in processed:
-                        f.append(pr['accuracy'][classes[c]])
+                        f.append(pr['loss'])
                         igs.append(pr['ig'])
                     x=moving_average(igs,n)
                     y=moving_average(f,n)
-                    plot(x,y,label=classes[c])
-                plt.title(p.data_recorders[task].name+' accuracy')
-                plt.legend(kys(classes),loc='upper left')
+                    plot(x,y,label=p.data_recorders[task].name)
+                plt.title('loss')
+                plt.legend(kys(p.data_recorders),loc='upper right')
                 plt.savefig(
-                    opj(save_path,
-                        p.data_recorders[task].name+'-'+get_safe_name(
-                            'accuracy')+'.pdf'),
+                    opj(save_path,get_safe_name('loss')+'.pdf'),
                     bbox_inches='tight')
-
-
-                figure(1);clf()
-                for c in classes:
-                    f=[]
-                    igs=[]
-                    for pr in processed:
-                        f.append(pr['f1_scores'][classes[c]])
-                        igs.append(pr['ig'])
-                    x=moving_average(igs,n)
-                    y=moving_average(f,n)
-                    plot(x,y,label=classes[c])
-                plt.title(p.data_recorders[task].name+' f1-scores')
-                plt.legend(kys(classes),loc='upper left')
-                plt.savefig(
-                    opj(save_path,p.data_recorders[
-                        task].name+'-'+get_safe_name(
-                            'f1-scores')+'.pdf'),
-                    bbox_inches='tight')
-
-
-                try:
-                    if len(processed)>1:
-                        fig=figure(1);clf()
-                        ax=fig.add_subplot(111)
-                        a=max(1,int(len(processed)*0.9))
-                        b=len(processed)
-                        c=[]
-                        for i in range(a,b):
-                            c.append(processed[i]['confusion_matrix'])
-                        c=na(c)
-                        c=c.sum(axis=0)
-                        c=np.round((100*c.astype(
-                            'float')/c.sum(axis=1)[:,np.newaxis])).astype(int)
-
-                        disp=ConfusionMatrixDisplay(
-                            confusion_matrix=c,
-                            display_labels=kys(classes))
-                        disp.plot(ax=ax,cmap=plt.cm.Blues)
-                        plt.title(
-                            d2s(p.data_recorders[task].name,'confusion_matrix',ig))
-                        plt.savefig(
-                            opj(save_path,
-                                p.data_recorders[task].name+'-'+get_safe_name(
-                                    'confusion_matrix.pdf')),
-                            bbox_inches='tight')
-                except KeyboardInterrupt:
-                    cr('*** KeyboardInterrupt ***')
-                    sys.exit()
-                except Exception as e:
-                    exc_type, exc_obj, exc_tb = sys.exc_info()
-                    file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                    print('Exception!')
-                    print(d2s(exc_type,file_name,exc_tb.tb_lineno)) 
-
-
-
-            figure(1)
-            clf()
-            for task in p.data_recorders:
-                processed=p.data_recorders[task].processed
-                f=[]
-                igs=[]
-                for pr in processed:
-                    f.append(pr['loss'])
-                    igs.append(pr['ig'])
-                x=moving_average(igs,n)
-                y=moving_average(f,n)
-                plot(x,y,label=p.data_recorders[task].name)
-            plt.title('loss')
-            plt.legend(kys(p.data_recorders),loc='upper right')
-            plt.savefig(
-                opj(save_path,get_safe_name('loss')+'.pdf'),
-                bbox_inches='tight')
-        """
-        except KeyboardInterrupt:
-            cr('*** KeyboardInterrupt ***')
-            sys.exit()
-        except Exception as e:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print('Exception!')
-            print(d2s(exc_type,file_name,exc_tb.tb_lineno)) 
-        """
+    
+    except KeyboardInterrupt:
+        cr('*** KeyboardInterrupt ***')
+        sys.exit()
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print('Exception!')
+        print(d2s(exc_type,file_name,exc_tb.tb_lineno)) 
+    
 
     #
     ##########################################################################
